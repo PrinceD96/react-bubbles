8c960c4618a59713807e62fec0236175dd815a1f
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-02-21 22:26:23 -0500

    Login working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner"
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  })
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+    setIsLoading(true);
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        console.log("axios POST", res.data)
+        sessionStorage.setItem("token", res.data.payload)
+        props.history.push("/bubble-page");
+        setIsLoading(false)
+      })
+      .catch(error => {
+        console.log("axios POST: Invalid credentials, try again.", error)
+        sessionStorage.removeItem("token")
+      });
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    < div className="login__container" >
+      {
+        isLoading ? (
+          <Loader
+            type="Grid"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000}
+            style={{ marginTop: "200px" }}
+          />
+        ) : (
+            <>
+              <h1>Welcome to the Bubble App!</h1>
+              <p>Login</p>
+              <form onSubmit={handleLogin}>
+                <input type="text" name="username" placeholder="username" value={credentials.username} onChange={handleChange} required />
+                <input type="password" name="password" placeholder="password" value={credentials.password} onChange={handleChange} required />
+                <button>Login</button>
+              </form>
+            </>
+          )}
+    </div >
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
